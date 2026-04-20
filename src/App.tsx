46c8470a54750dd1bc34b159b07dfb54a5df4c19
Vidhi Thakur <thakurvidhi997@gmail.com>
2026-04-20 19:02:36 +0530

    kanban board basic layout UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
-import './App.css'
+import "./App.css";
+import Appbar from "./components/layout/Appbar";
 
 function App() {
-
   return (
     <>
-      
+      <section id="left-sidebar"></section>
+      <section id="main-content">
+        <Appbar />
+      </section>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/layout/Appbar.tsx
@@ -0,0 +1,10 @@
+import '../styles/Appbar.css';
+
+function Appbar() {
+  return (
+    <div className="appbar">
+    </div>
+  )
+}
+
+export default Appbar
