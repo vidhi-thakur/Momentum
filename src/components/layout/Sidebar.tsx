ea6958eb6fc81e967e6afd05bf1f04dd87f26c77
Vidhi Thakur <thakurvidhi997@gmail.com>
2026-04-22 22:07:57 +0530

    updated UI for sidebar
    - added logo as link

--- src/components/layout/Sidebar.tsx
+++ src/components/layout/Sidebar.tsx
@@ -1,9 +1,14 @@
-import '../styles/Sidebar.css';
+import "../styles/Sidebar.css";
+import { Link } from 'react-router-dom';
 
 function Sidebar() {
   return (
-    <div>Sidebar</div>
-  )
+    <div className="sidebar">
+      <Link to="/">
+      <img src="/logo.png" alt="avatar" width={45} />
+      </Link>
+    </div>
+  );
 }
 
-export default Sidebar
+export default Sidebar;
